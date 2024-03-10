1dec0cb1128e3bcdd04a996eb6dc7ebc0b6a6220
Thaïs Brun <travailthais@outlook.fr>
2024-03-10 17:32:40 +0100

    prise en main vue JS

--- globetrotter/src/main.js
+++ globetrotter/src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+import 'foundation-sites/dist/css/foundation.min.css';
+import 'foundation-sites/dist/js/foundation.min.js';
 import { createApp } from 'vue'
 import App from './App.vue'
 
