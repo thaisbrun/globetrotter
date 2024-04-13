ed2e74ed149781d3b8246970ffcd82a5e281199f
Thaïs Brun <travailthais@outlook.fr>
2024-04-13 21:02:26 +0200

    prise en main vue JS

--- globetrotter/src/App.vue
+++ globetrotter/src/App.vue
@@ -1,19 +1,17 @@
 <template>
 
-  <div :class="{'text-green': user.age > 20}">
-    <h1> {{ user.name }} </h1>
-  </div>
 </template>
 
 <script setup>
 import navbar from '@/components/navbar.vue'
 //Ref rend la variable réactive
-import { reactive } from 'vue';
+import {watch, ref} from 'vue';
 
-const user = reactive({
-  name: 'thais',
-  age: 21,
-});
+const name = ref('Thaïs');
+const age = ref(21);
+watch(name, (newValue, oldValue) =>{
+  console.log(newValue, oldValue);
+})
 </script>
 
 <style scoped>
--- globetrotter/src/components/navbar.vue
+++ globetrotter/src/components/navbar.vue
@@ -1,6 +1,7 @@
 <template>
-  <navbar />
-</template>
-
+  <h1>Hello</h1></template>
+{{name }}
 <script setup>
+import {ref} from 'vue';
+const name = ref('');
 </script>
