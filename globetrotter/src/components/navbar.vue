0b41bccd3cce99eaf5603a511971e15b07de01be
Thaïs Brun <travailthais@outlook.fr>
2024-04-14 10:20:09 +0200

    prise en main vue JS

--- globetrotter/src/components/navbar.vue
+++ globetrotter/src/components/navbar.vue
@@ -1,7 +1,13 @@
 <template>
   <h1>Hello</h1></template>
 {{name }}
+<input type="text" v-model="name" @input="handleUpdate">
 <script setup>
 import {ref} from 'vue';
+const emit = defineEmits(['updateName'])
 const name = ref('');
+
+const handleUpdate = (event) => {
+  console.log(event.target.value)
+}
 </script>
