2886036515cb97d18833845c18572ae420b2e195
Thaïs Brun <travailthais@outlook.fr>
2024-04-20 19:52:15 +0200

    prise en main vue JS

--- globetrotter/src/App.vue
+++ globetrotter/src/App.vue
@@ -1,17 +1,21 @@
 <template>
+  <h1> install </h1>
+  <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+    <div class="pt-6 space-y-4">
+      <blockquote>
+        <p class="text-lg font-medium">
+          “Tailwind CSS is the only framework that I've seen scale
+          on large teams. It’s easy to customize, adapts to any design,
+          and the build size is tiny.”
+        </p>
+      </blockquote>
+  </div>
+</figure>
 
 </template>
 
 <script setup>
 import navbar from '@/components/navbar.vue'
-//Ref rend la variable réactive
-import {watch, ref} from 'vue';
-
-const name = ref('Thaïs');
-const age = ref(21);
-watch(name, (newValue, oldValue) =>{
-  console.log(newValue, oldValue);
-})
 </script>
 
 <style scoped>
--- globetrotter/src/main.js
+++ globetrotter/src/main.js
@@ -1,6 +1,4 @@
 import './assets/main.css'
-import 'foundation-sites/dist/css/foundation.min.css';
-import 'foundation-sites/dist/js/foundation.min.js';
 import { createApp } from 'vue'
 import App from './App.vue'
 
