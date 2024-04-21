6edadfb4f32d0e018996da5ec7130b14f264f23f
Thaïs Brun <travailthais@outlook.fr>
2024-04-21 18:22:49 +0200

    ajout tailwind

--- globetrotter/src/App.vue
+++ globetrotter/src/App.vue
@@ -1,16 +1,5 @@
 <template>
-  <h1> install </h1>
-  <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-    <div class="pt-6 space-y-4">
-      <blockquote>
-        <p class="text-lg font-medium">
-          “Tailwind CSS is the only framework that I've seen scale
-          on large teams. It’s easy to customize, adapts to any design,
-          and the build size is tiny.”
-        </p>
-      </blockquote>
-  </div>
-</figure>
+    <div class="text-green-700 text-xl">Hello, from TailwindCSS!</div>
 
 </template>
 
