fc556f8278f5619b7503397eec09ad709ec9e013
Thaïs Brun <travailthais@outlook.fr>
2024-08-19 21:22:11 +0200

    Serveur ok + début du projet

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//Routes : request et response
+router.get("/", (req, res) => {
+    res.json({ message: "top "});
+})
+
+router.post("/", (req, res) => {
+    res.json({ message: req.body.message });
+})
+
+module.exports = router
+
